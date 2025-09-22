33b02c1a9bdc99503b75cb2e9da12dfe701961e2
Pritam Kumar <pritam.up2mark@gmail.com>
2025-09-22 16:33:46 +0530

    Fix webhooks HMAC verification & compliance endpoints

--- test.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import dotenv from "dotenv";
-import { shopifyApp } from "@shopify/shopify-app-express";
-
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => res.send('Server running!'));
-app.listen(3000, () => console.log('Server started on 3000'));
